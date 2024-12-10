cb454ad88523ff0ae254c4e88623cb3929210cca
VopRos-ZP <zolpavel366@gmail.com>
2024-12-10 16:29:24 +0300

    Добавление табов

--- Utils/Db.cs
+++ Utils/Db.cs
@@ -1,9 +1,20 @@
-﻿using VentSystems.Model;
+﻿using System.Collections.Generic;
+using VentSystems.Model;
 
 namespace VentSystems.Utils
 {
     public class Db
     {
         public static VentsEntities Entities = new VentsEntities();
+
+        public static List<string> TableNames = new List<string>
+        {
+            "Продукты",
+            "Пользователи",
+            "Роли",
+            "Страны",
+            "Склады",
+            "Поставщики",
+        };
     }
 }
--- Windows/AdminWindow.xaml.cs
+++ Windows/AdminWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using VentSystems.Model;
 using VentSystems.Utils;
 using VentSystems.Windows.Upsert;
@@ -9,19 +11,46 @@ namespace VentSystems.Windows
 {
     public partial class AdminWindow : Window
     {
+        private readonly List<string> _excludeNames = new List<string>();
+
         public AdminWindow(Users user)
         {
             InitializeComponent();
+
+            InitExcludeNames();
+
+            InitDataGrid(UsersList,Db.Entities.Users.ToList());
+            InitDataGrid(RolesList,Db.Entities.Roles.ToList());
+            InitDataGrid(SuppliersList,Db.Entities.Suppliers.ToList());
+            InitDataGrid(StoragesList,Db.Entities.Storages.ToList());
+            InitDataGrid(CountriesList,Db.Entities.Countries.ToList());
             
             UpdateProductsList(Db.Entities.Products.ToList());
-            
+
             FindBox.TextChanged += (s, e) =>
             {
-                var filter = Db.Entities.Products.ToList().FindAll(p => p.Name.ToLower().Contains(FindBox.Text.ToLower()));
+                var filter = Db.Entities.Products.ToList()
+                    .FindAll(p => p.Name.ToLower().Contains(FindBox.Text.ToLower()));
                 UpdateProductsList(filter);
             };
         }
-        
+
+        private void InitExcludeNames()
+        {
+            _excludeNames.Add("Users");
+            _excludeNames.Add("Roles");
+            _excludeNames.Add("Products");
+            _excludeNames.Add("Storages");
+            _excludeNames.Add("Suppliers");
+            _excludeNames.Add("Countries");
+        }
+
+        private void InitDataGrid<T>(DataGrid dataGrid, List<T> source)
+        {
+            dataGrid.CanUserSortColumns = true;
+            dataGrid.ItemsSource = source;
+        }
+
         private void UpdateProductsList(List<Products> products)
         {
             ProductsList.ItemsSource = products;
@@ -42,13 +71,22 @@ namespace VentSystems.Windows
         {
             new UpsertProductWindow(product).ShowDialog();
         }
-        
+
         private void DeleteBtn_OnClick(object sender, RoutedEventArgs e)
         {
             if (!(ProductsList.SelectedItem is Products product)) return;
             Db.Entities.Products.Remove(product);
             Db.Entities.SaveChanges();
         }
+
+        private void OnAutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            var propertyDescriptor = (PropertyDescriptor)e.PropertyDescriptor;
+            e.Column.Header = propertyDescriptor.DisplayName;
+            if (_excludeNames.Contains(propertyDescriptor.DisplayName))
+            {
+                e.Cancel = true;
+            }
+        }
     }
-    
 }
